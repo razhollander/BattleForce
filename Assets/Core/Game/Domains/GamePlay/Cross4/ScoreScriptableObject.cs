50fde46343b077fa44f0f4b024e6255740928fce
razhollander <50023823+razhollander@users.noreply.github.com>
2026-01-18 10:45:50 +0200

    remove cross4

--- Assets/Core/Game/Domains/GamePlay/Cross4/ScoreScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using Utils;
-
-[CreateAssetMenu(fileName = "ScoreData", menuName = "Game/ScoreData", order = 1)]
-public class ScoreScriptableObject : ScriptableObject
-{
-    public SerializableDictionary<int, int> PlayerIdToScore;
-    //private <int, int> _playerIdToScore;
-}
