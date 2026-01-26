daf22a0190d26e7622951409106816ccd5d465d8
razhollander <50023823+razhollander@users.noreply.github.com>
2026-01-26 16:29:09 +0200

    cr fixes

--- Assets/Core/Game/Domains/GamePlay/Presentation/Features/Environment/Walls/Scripts/EnvironmentWallView.cs
+++ Assets/Core/Game/Domains/GamePlay/Presentation/Features/Environment/Walls/Scripts/EnvironmentWallView.cs
@@ -1,4 +1,3 @@
-using DG.Tweening;
 using UnityEngine;
 
 namespace Core.Game.Domains.GamePlay.Presentation.Features.Environment.Walls.Scripts
@@ -6,21 +5,10 @@ namespace Core.Game.Domains.GamePlay.Presentation.Features.Environment.Walls.Scr
     public class EnvironmentWallView : MonoBehaviour
     {
         [SerializeField] private MeshFilter _meshFilter;
-        [SerializeField] private MeshRenderer _meshRenderer;
 
         public void SetMesh(Mesh mesh)
         {
             _meshFilter.sharedMesh = mesh;
-        }
-
-        public void SetColor(Color color)
-        {
-            _meshRenderer.material.color = color;
-        }
-
-        public void AnimateBounce()
-        {
-            transform.DOScale(1.2f, 0.15f).SetLoops(2, LoopType.Yoyo);
         }
     }
 }
