7ca57d8bfc0c732e98859e1f325dcc819ffca315
razhollander <50023823+razhollander@users.noreply.github.com>
2026-02-04 16:45:01 +0200

    self cr fixes

--- Assets/Core/Game/Domains/GamePlay/Simulation/Scripts/Services/PlayersForcesService/PlayersVelocityService.cs
+++ Assets/Core/Game/Domains/GamePlay/Simulation/Scripts/Services/PlayersForcesService/PlayersVelocityService.cs
@@ -31,24 +31,12 @@ namespace Core.Game.Domains.GamePlay.Simulation.Scripts.Services.PlayersForcesSe
                 playerSpaceshipState.Transform.AngularVelocity = 0;
             }
 
-            // 4. Apply Rotation in Degrees
             if (playerSpaceshipState.Transform.AngularVelocity != 0)
             {
                 // Velocity (deg/s) * Time (s) = Rotation for this frame (deg)
                 var rotationDegrees = playerSpaceshipState.Transform.AngularVelocity * deltaTime;
                 playerSpaceshipState.Transform.Direction.Rotate(rotationDegrees);
             }
-            
-            //playerSpaceshipState.Transform.AngularVelocity -= deltaTIme * _simulationGamePlayConfig.PlayerSpaceship.SpinDampingPerSecond;
-            //
-            //     if (playerSpaceshipState.Transform.AngularVelocity < 0)
-            //     {
-            //         playerSpaceshipState.Transform.AngularVelocity = 0;
-            //     }
-            //     else
-            //     {
-            //         playerSpaceshipState.Transform.Direction.Rotate(playerSpaceshipState.Transform.AngularVelocity);
-            //     }
         }
         
         public void DeceleratePlayerVelocity(PlayerSpaceshipStateS2C playerSpaceshipState, float deltaTIme)
@@ -56,49 +44,6 @@ namespace Core.Game.Domains.GamePlay.Simulation.Scripts.Services.PlayersForcesSe
             var playerMovementSpeed = playerSpaceshipState.Transform.Velocity.Length();
             var newSpeed = playerMovementSpeed - _simulationGamePlayConfig.PlayerSpaceship.VelocityDecelerationPerSecond * deltaTIme;
             playerSpaceshipState.Transform.Velocity = playerSpaceshipState.Transform.Velocity.Normalize() * newSpeed;
-
-            // if (playerSpaceshipState.IsEngineOn) // add a force applied to the player every frame, and remove it when reaching the target speed
-            // {
-            //     var isPlayerAtTargetMovementSpeed = Mathf.Approximately(playerMovementSpeed, targetMovementSpeed);
-            //     if (isPlayerAtTargetMovementSpeed)
-            //     {
-            //         newSpeed = targetMovementSpeed;
-            //     }
-            //     else
-            //     {
-            //         var isAboveTargetMovementSpeed = playerMovementSpeed > targetMovementSpeed;
-            //         if (isAboveTargetMovementSpeed)
-            //         {
-            //             newSpeed = playerMovementSpeed - _simulationGamePlayConfig.PlayerSpaceship.VelocityDampingPerSecond * deltaTIme;
-            //         }
-            //         else
-            //         {
-            //             var engineForce = _simulationGamePlayConfig.PlayerSpaceship.EngineAcceleration * deltaTIme * playerSpaceshipState.Transform.Direction;
-            //             AddForceToPlayer(playerSpaceshipState, engineForce);
-            //             newSpeed = playerSpaceshipState.Transform.Velocity.Length();
-            //             var didPassTargetMovementSpeed = newSpeed > targetMovementSpeed;
-            //
-            //             if (didPassTargetMovementSpeed)
-            //             {
-            //                 newSpeed = targetMovementSpeed;
-            //             }
-            //         }
-            //     }
-            // }
-            // else
-            // {
-            //     var isIdle = playerMovementSpeed <= _simulationGamePlayConfig.PlayerSpaceship.IdleMovementSpeed;
-            //     if (isIdle)
-            //     {
-            //         newSpeed = playerMovementSpeed;
-            //     }
-            //     else
-            //     {
-            //         newSpeed = playerMovementSpeed - _simulationGamePlayConfig.PlayerSpaceship.VelocityDampingPerSecond * deltaTIme;
-            //     }
-            // }
-            
-            //playerSpaceshipState.Transform.Velocity = playerSpaceshipState.Transform.Velocity.Normalize() * newSpeed;
         }
     }
 }
