91dacd854ea5754f5a7e04889e85684f86582de4
razhollander <50023823+razhollander@users.noreply.github.com>
2026-06-02 19:11:20 +0300

    remove junk

--- update5.sh
+++
@@ -1,1 +0,0 @@
-sed -i '/public void UpdateEffects();/a \        void UpdateTargetsPositionOnPlayer(UnityEngine.Vector3 playerHeartPosition);' Assets/Core/Game/Domains/GamePlay/Presentation/Match/Features/LockOnHeartSights/Scripts/ILockOnTargetEffectController.cs
