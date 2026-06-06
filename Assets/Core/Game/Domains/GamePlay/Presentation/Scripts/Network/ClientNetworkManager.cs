a6b58725e50c0a687b476985e42e406b570c27f3
razhollander <50023823+razhollander@users.noreply.github.com>
2026-06-06 23:07:09 +0300

    ping

--- Assets/Core/Game/Domains/GamePlay/Presentation/Scripts/Network/ClientNetworkManager.cs
+++ Assets/Core/Game/Domains/GamePlay/Presentation/Scripts/Network/ClientNetworkManager.cs
@@ -20,7 +20,8 @@ namespace Core.Game.Domains.GamePlay.Presentation.Scripts.Network
         private readonly ICommandFactory _commandFactory;
         private readonly IUpdateSubscriptionService _updateSubscriptionService;
         private readonly NetworkC2SPacketsSender _packetsSender;
-        private readonly GUIStyle _guiStyle;
+        private GUIStyle _guiStyle;
+
         private string _playerName;
         public bool IsPeerConnected { get; private set; }
         public int Ping => _packetsSender.Peer.Ping;
@@ -38,10 +39,6 @@ namespace Core.Game.Domains.GamePlay.Presentation.Scripts.Network
                 AutoRecycle = true,
                 IPv6Enabled = false
             };
-            
-            _guiStyle = new GUIStyle();
-            _guiStyle.fontSize = 10;
-            _guiStyle.normal.textColor = Color.white;
         }
 
         public void ConenctToServerPeer(string ipAddress, int port, string playerName)
@@ -134,7 +131,23 @@ namespace Core.Game.Domains.GamePlay.Presentation.Scripts.Network
 
         public void ManagedOnGUI()
         {
-//            GUI.Label(new Rect(10, 10, 400, 30), "Local Host Ping: "+_packetsListener.PingToLocalHost, _guiStyle);
+            TryInitGuiStyle(); // ugly, but must be called inside ManagedOnGUI
+            GUILayout.Box("Local Host Ping: "+_packetsListener.PingToLocalHost+$", another way: {Ping}", _guiStyle);
+            
+        }
+
+        private void TryInitGuiStyle()
+        {
+            if (_guiStyle != null)
+            {
+                return;
+            }
+            
+            _guiStyle = new GUIStyle(GUI.skin.box); 
+            _guiStyle.normal.background = Texture2D.whiteTexture;
+            _guiStyle.fontSize = 16; 
+            _guiStyle.fontStyle = FontStyle.Bold;
+            _guiStyle.normal.textColor = Color.black;
         }
 
         public void ManagedOnDrawGizmos()
