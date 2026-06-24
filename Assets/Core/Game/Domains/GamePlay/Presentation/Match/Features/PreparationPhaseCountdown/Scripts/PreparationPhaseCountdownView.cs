f7566d83f355de8d768b97d50adf2f40ff2af3ca
razhollander <50023823+razhollander@users.noreply.github.com>
2026-06-25 00:26:11 +0300

    wip powerups

--- Assets/Core/Game/Domains/GamePlay/Presentation/Match/Features/PreparationPhaseCountdown/Scripts/PreparationPhaseCountdownView.cs
+++ Assets/Core/Game/Domains/GamePlay/Presentation/Match/Features/PreparationPhaseCountdown/Scripts/PreparationPhaseCountdownView.cs
@@ -4,18 +4,21 @@ namespace Core.Game.Domains.GamePlay.Presentation.Match.Features.PreparationPhas
 {
     public class PreparationPhaseCountdownView : MonoBehaviour
     {
-        [SerializeField] private Animator _animator;
-        [SerializeField] private string _countdownAnimationStateName = "Countdown";
+        [SerializeField] private Animation _animation; 
+        [SerializeField] private string _countdownAnimationClipName = "PreparationPhaseCountdown";
 
         public void Play(float elapsedTimeInSeconds)
         {
             gameObject.SetActive(true);
-            _animator.Play(_countdownAnimationStateName, 0, 0f);
-            _animator.Update(elapsedTimeInSeconds);
+            AnimationState state = _animation[_countdownAnimationClipName];
+            _animation.Play(_countdownAnimationClipName);
+            state.time = elapsedTimeInSeconds;
+            _animation.Sample();
         }
 
         public void Stop()
         {
+            _animation.Stop(); 
             gameObject.SetActive(false);
         }
     }
